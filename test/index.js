30cf4faae0c2366e55c16880d7aeb7afc5c100ee
tunnckoCore <mameto_100@mail.bg>
2015-04-05 18:03:41 +0300

    move to `assertit`

--- test/index.js
+++ test/index.js
@@ -8,15 +8,14 @@
 'use strict';
 
 var fs = require('fs');
-var test = require('testit');
-var assert = require('assert');
+var test = require('assertit');
 var transform = require('../index');
 
 test('should transform legacy css to css (.render)', function(done) {
   var actual = transform.render('body {\n  font-size: 12px;\n}');
   var expected = 'body {\n  font-size: 12px;\n}';
 
-  assert.strictEqual(actual, expected);
+  test.equal(actual, expected);
   done();
 });
 
@@ -24,7 +23,7 @@ test('should support variables (.render)', function(done) {
   var actual = transform.render(':root {\n  --purple: #e3fd5a;\n}\nbody {\n  color: var(--purple);\n}');
   var expected = 'body {\n  color: #e3fd5a;\n}';
 
-  assert.strictEqual(actual, expected);
+  test.equal(actual, expected);
   done();
 });
 
@@ -32,7 +31,7 @@ test('should support calc (.render)', function(done) {
   var actual = transform.render('pre {\n  margin: calc(50px * 2);\n}');
   var expected = 'pre {\n  margin: 100px;\n}';
 
-  assert.strictEqual(actual, 'pre {\n  margin: 100px;\n}');
+  test.equal(actual, 'pre {\n  margin: 100px;\n}');
   done();
 });
 
@@ -40,7 +39,7 @@ test('should render from a given filepath, synchronously', function(done) {
   var actual = transform.renderFile('./test/fixtures/myth.in.css');
   var expected = fs.readFileSync('./test/fixtures/myth.out.css', 'utf8');
 
-  assert.strictEqual(actual, expected);
+  test.equal(actual, expected);
   done();
 });
 
@@ -49,7 +48,7 @@ test('should render myth file asynchronously (promise)', function(done) {
   var expected = fs.readFileSync('./test/fixtures/myth.out.css', 'utf8');
 
   promise.then(function(actual) {
-    assert.strictEqual(actual, expected);
+    test.equal(actual, expected);
     done();
   });
 });
