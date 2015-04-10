9620542b789233550ae2e6031af340fddac18251
tunnckoCore <mameto_100@mail.bg>
2015-04-10 18:38:48 +0300

    run update, close #1

--- index.js
+++ index.js
@@ -10,22 +10,10 @@
 var fs = require('fs');
 var path = require('path');
 var myth = require('myth');
-var bluebird = require('bluebird');
-var readFile = bluebird.promisify(fs.readFile);
-var readFileSync = fs.readFileSync;
 
 exports.name = 'myth';
 exports.outputFormat = 'css';
 
-exports.render = function _render(str, opts) {
-  return myth(str, opts);
-};
-exports.renderFile = function _renderFile(filepath, opts) {
-  var input = readFileSync(path.resolve(filepath), 'utf8');
-  return exports.render(input, opts);
-};
-exports.renderFileAsync = function _renderFileAsync(filepath, opts) {
-  return readFile(filepath, 'utf8').then(function(data) {
-    return exports.render(data, opts);
-  });
+exports.render = function _render(str, options) {
+  return myth(str, options);
 };
--- test/index.js
+++ test/index.js
@@ -9,13 +9,14 @@
 
 var fs = require('fs');
 var test = require('assertit');
-var transform = require('../index');
+var transformer = require('jstransformer');
+var transform = transformer(require('../index'));
 
 test('should transform legacy css to css (.render)', function(done) {
   var actual = transform.render('body {\n  font-size: 12px;\n}');
   var expected = 'body {\n  font-size: 12px;\n}';
 
-  test.equal(actual, expected);
+  test.equal(actual.body, expected);
   done();
 });
 
@@ -23,7 +24,7 @@ test('should support variables (.render)', function(done) {
   var actual = transform.render(':root {\n  --purple: #e3fd5a;\n}\nbody {\n  color: var(--purple);\n}');
   var expected = 'body {\n  color: #e3fd5a;\n}';
 
-  test.equal(actual, expected);
+  test.equal(actual.body, expected);
   done();
 });
 
@@ -31,24 +32,24 @@ test('should support calc (.render)', function(done) {
   var actual = transform.render('pre {\n  margin: calc(50px * 2);\n}');
   var expected = 'pre {\n  margin: 100px;\n}';
 
-  test.equal(actual, 'pre {\n  margin: 100px;\n}');
+  test.equal(actual.body, 'pre {\n  margin: 100px;\n}');
   done();
 });
 
 test('should render from a given filepath, synchronously', function(done) {
-  var actual = transform.renderFile('./test/fixtures/myth.in.css');
-  var expected = fs.readFileSync('./test/fixtures/myth.out.css', 'utf8');
+  var actual = transform.renderFile('./test/fixture.css');
+  var expected = fs.readFileSync('./test/expected.css', 'utf8');
 
-  test.equal(actual, expected);
+  test.equal(actual.body, expected);
   done();
 });
 
 test('should render myth file asynchronously (promise)', function(done) {
-  var promise = transform.renderFileAsync('./test/fixtures/myth.in.css');
-  var expected = fs.readFileSync('./test/fixtures/myth.out.css', 'utf8');
+  var promise = transform.renderFileAsync('./test/fixture.css');
+  var expected = fs.readFileSync('./test/expected.css', 'utf8');
 
   promise.then(function(actual) {
-    test.equal(actual, expected);
+    test.equal(actual.body, expected);
     done();
   });
 });
