b7368d4b6708f7f756d78c2a4e8bb8fd7730d22c
Rob Loach <robloach@gmail.com>
2015-08-04 03:23:38 -0400

    Clean up the diff

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 var myth = require('myth');
 
 exports.name = 'myth';
-exports.outputFormat = 'css';
 exports.inputFormats = ['css', 'myth'];
+exports.outputFormat = 'css';
 
 exports.render = function _render(str, options) {
   return myth(str, options);
