63829fd0feb398b7dc146ab29aac8be0a17ca685
Rob Loach <robloach@gmail.com>
2015-08-04 03:05:54 -0400

    Update Boilerplate and Myth

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-/**
- * jstransformer-myth <https://github.com/jstransformers/jstransformer-myth>
- *
- * Copyright (c) 2015 Charlike Mike Reagent, contributors.
- * Released under the MIT license.
- */
-
 'use strict';
 
 var myth = require('myth');
 
 exports.name = 'myth';
-exports.inputFormats = ['css', 'myth'];
 exports.outputFormat = 'css';
 
 exports.render = function _render(str, options) {
